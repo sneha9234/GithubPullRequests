abab6b332aa8e0be707c2694bb05f2752328d2da
Sneha <sneha@tamasha.live>
2022-08-14 00:29:39 +0530

    unit test

---
+++ app/src/test/java/com/example/pullrequests/viewmodel/MainActivityViewModelTest.kt
@@ -0,0 +1,23 @@
+package com.example.pullrequests.viewmodel
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import junit.framework.TestCase
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class MainActivityViewModelTest: TestCase(){
+
+    private lateinit var viewModel: MainActivityViewModel
+
+    @Before
+    public override fun setUp() {
+        super.setUp()
+    }
+
+    @Test
+    fun testMainActiviyViewModel(){
+      //  viewModel.getAccessToken()
+    }
+}
